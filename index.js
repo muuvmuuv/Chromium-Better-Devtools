e60cf9d7cb1ddcafc9466070344d5468ea8e6b73
Marvin Heilemann <m.heilemann@protonmail.com>
2021-09-28 19:46:12 +0200

    Minify and adjust position

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "/style.css", true);
-xhr.onload = function () {
-  console.log("Loading", xhr.readyState);
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      console.log("Loaded style");
-      console.log(xhr.responseText);
-      chrome.devtools.panels.applyStyleSheet(xhr.responseText);
-    } else {
-      console.error(xhr.statusText);
-    }
-  }
-};
-xhr.onerror = function () {
-  console.error(xhr.statusText);
-};
-xhr.send(null);
+e=new XMLHttpRequest;e.open("GET","/style.css",!0),e.onload=function(){4===e.readyState&&200===e.status&&chrome.devtools.panels.applyStyleSheet(e.responseText)},e.send(null)
