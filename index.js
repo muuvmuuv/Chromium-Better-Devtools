fed14acefd2fa4c1198e8fb42710d2bf92cc7533
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2024-04-23 09:27:05 +0200

    Style not applied since Brave update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
     const request = await fetch("/style.css");
     const response = await request.text();
     chrome.devtools.panels.applyStyleSheet(response);
+    console.log("Applied dev-tools stylesheet");
   } catch (error) {
     console.error(error);
   }
